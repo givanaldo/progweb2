5c657783ddc2009c73789d164a232a3d6098f791
Givanaldo Rocha <givanaldo@gmail.com>
2017-09-14 16:51:24 -0300

    projeto de exemplo usado nas aulas

---
+++ progweb-maven-projeto/src/main/java/ifrn/progweb/model/TipoEmpresa.java
@@ -0,0 +1,30 @@
+package ifrn.progweb.model;
+
+public enum TipoEmpresa {
+	ME,
+	MEI,
+	EIRELI,
+	LTDA,
+	SA;
+
+	/*
+	ME ("Microempresa"),
+	MEI ("Microempreendedor Individual"),
+	EIRELI ("Empresa Individual de Responsabilidade Limitada"),
+	LTDA ("Sociedade Limitada"),
+	SA ("Sociedade Anônima");
+	
+	private String descricao;
+
+	private TipoEmpresa(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}*/
+}
