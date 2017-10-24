d0073f7b86e1d20ea59158488186d37a0947ff12
Givanaldo Rocha <givanaldo@gmail.com>
2017-10-24 17:20:41 -0200

    Mudanças na view

--- testes-jpql/src/main/java/view/TestJpql.java
+++ testes-jpql/src/main/java/view/TestJpql.java
@@ -1,8 +1,14 @@
 package view;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import model.JpaUtil;
+import model.TotalCarroPorAno;
+import model.Veiculo;
 
 public class TestJpql {
 	
@@ -10,7 +16,46 @@ public class TestJpql {
 		
 		EntityManager manager = JpaUtil.getEntityManager();
 		
-		// Consulta simples
+		// Consulta tipada com parâmetros
+		TypedQuery<Veiculo> queryVeiculo = 
+				manager.createQuery("from Veiculo where anoModelo >= :ano and valor <= :preco", Veiculo.class);
+		queryVeiculo.setParameter("ano", 2009);
+		queryVeiculo.setParameter("preco", new BigDecimal(25000));
+		List<Veiculo> veiculos = queryVeiculo.getResultList();
+		for (Veiculo v : veiculos) {
+			System.out.println(v.getCodigo() + " - " + v.getFabricante() + " " + 
+		                       v.getModelo() + " (" + v.getAnoModelo() + "): R$ " + v.getValor());
+		}
+		
+		// Projeções
+		TypedQuery<String> queryProprietario = manager.createQuery("select nome from Proprietario", String.class);
+		List<String> proprietarios = queryProprietario.getResultList();
+		for (String p : proprietarios) {
+			System.out.println(p);
+		}
+		
+		TypedQuery<Object[]> queryValores = manager.createQuery("select modelo, valor from Veiculo", Object[].class);
+		List<Object[]> valores = queryValores.getResultList();
+		for (Object[] resultado : valores) {
+			System.out.println(resultado[0] + ": R$ " + resultado[1]);
+		}
+		
+		// Funções de agregação
+		TypedQuery<TotalCarroPorAno> queryTotal = 
+				manager.createQuery("select new model.TotalCarroPorAno(v.anoFabricacao, "
+						+ "avg(v.valor), count(v)) from Veiculo v " 
+						+ "group by v.anoFabricacao", TotalCarroPorAno.class);
+		List<TotalCarroPorAno> resultado = queryTotal.getResultList();
+		for (TotalCarroPorAno v : resultado) {
+			System.out.println("Ano: " + v.getAnoFabricacao() + 
+							   " - Preço médio: R$ " + v.getMediaPreco() + 
+							   " - Quantidade: " + v.getQuantidadeCarros());
+		}
+		
+		TypedQuery<Long> queryTotalVeiculos = 
+				manager.createQuery("select count(v) from Veiculo v", Long.class);
+		Long totalVeiculos = queryTotalVeiculos.getSingleResult();
+		System.out.println("Total de veículos: " + totalVeiculos);
 		
 	}
 
--- testes-jpql/src/main/resources/META-INF/sql/dados-iniciais.sql
+++ testes-jpql/src/main/resources/META-INF/sql/dados-iniciais.sql
@@ -12,5 +12,19 @@ insert into acessorio (descricao) values ('Sensor de estacionamento');
 
 insert into veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor, tipo_combustivel, data_cadastro, cod_proprietario) values ('Fiat', 'Palio', 2010, 2010, 15000, 'GASOLINA', '2017-03-02', 1);
 insert into veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor, tipo_combustivel, data_cadastro, cod_proprietario) values ('Chevrolet', 'Onix', 2014, 2015, 35250, 'BICOMBUSTIVEL', '2017-03-02', 5);
-insert into veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor, tipo_combustivel, data_cadastro, cod_proprietario) values ('VW', 'Gol', 2016, 2016, 41300, 'BICOMBUSTIVEL', '2017-03-02', 2);
-      
+insert into veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor, tipo_combustivel, data_cadastro, cod_proprietario) values ('VW', 'Gol', 2016, 2016, 41300, 'BICOMBUSTIVEL', '2017-03-02', 2);     
+insert into veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor, tipo_combustivel, data_cadastro, cod_proprietario) values ('Fiat', 'Punto', 2011, 2012, 22000, 'ÁLCOOL', '2017-03-02', 3);
+insert into veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor, tipo_combustivel, data_cadastro, cod_proprietario) values ('Fiat', 'Uno', 2014, 2015, 25590, 'BICOMBUSTIVEL', '2017-03-02', 4);
+
+insert into veiculo_acessorio (cod_veiculo, cod_acessorio) values (1, 2);
+insert into veiculo_acessorio (cod_veiculo, cod_acessorio) values (1, 5);
+insert into veiculo_acessorio (cod_veiculo, cod_acessorio) values (2, 1);
+insert into veiculo_acessorio (cod_veiculo, cod_acessorio) values (2, 2);
+insert into veiculo_acessorio (cod_veiculo, cod_acessorio) values (2, 3);
+insert into veiculo_acessorio (cod_veiculo, cod_acessorio) values (2, 4);
+insert into veiculo_acessorio (cod_veiculo, cod_acessorio) values (2, 5);
+insert into veiculo_acessorio (cod_veiculo, cod_acessorio) values (3, 1);
+insert into veiculo_acessorio (cod_veiculo, cod_acessorio) values (4, 2);
+insert into veiculo_acessorio (cod_veiculo, cod_acessorio) values (4, 3);
+insert into veiculo_acessorio (cod_veiculo, cod_acessorio) values (4, 4);
+
