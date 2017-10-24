19bb6be09c8efae20d7d64f1c2ba9762beba97e4
Givanaldo Rocha <givanaldo@gmail.com>
2017-10-24 13:05:33 -0200

    atualização do script sql

--- testes-jpql/src/main/resources/META-INF/sql/dados-iniciais.sql
+++ testes-jpql/src/main/resources/META-INF/sql/dados-iniciais.sql
@@ -10,9 +10,7 @@ insert into acessorio (descricao) values ('Central multimídia');
 insert into acessorio (descricao) values ('Câmera de ré'); 
 insert into acessorio (descricao) values ('Sensor de estacionamento');
 
-insert into veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor, tipo_combustivel, data_cadastro, cod_proprietario)
-       values ('Fiat', 'Palio', 2010, 2010, 15000.00, 'GASOLINA', '2017-03-02', 1);
-insert into veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor, tipo_combustivel, data_cadastro, cod_proprietario)
-       values ('Chevrolet', 'Onix', 2014, 2015, 35250.00, 'BICOMBUSTIVEL', '2017-03-02', 5);
-insert into veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor, tipo_combustivel, data_cadastro, cod_proprietario)
-       values ('VW', 'Gol', 2016, 2016, 41300.00, 'BICOMBUSTIVEL', '2017-03-02', 2);
+insert into veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor, tipo_combustivel, data_cadastro, cod_proprietario) values ('Fiat', 'Palio', 2010, 2010, 15000, 'GASOLINA', '2017-03-02', 1);
+insert into veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor, tipo_combustivel, data_cadastro, cod_proprietario) values ('Chevrolet', 'Onix', 2014, 2015, 35250, 'BICOMBUSTIVEL', '2017-03-02', 5);
+insert into veiculo (fabricante, modelo, ano_fabricacao, ano_modelo, valor, tipo_combustivel, data_cadastro, cod_proprietario) values ('VW', 'Gol', 2016, 2016, 41300, 'BICOMBUSTIVEL', '2017-03-02', 2);
+      
