22c38a4c0ec2b9704fad5942e85059f4494a2749
givanaldo <givanaldo@10.208.12.47>
2017-11-21 15:02:06 -0300

    Injecao de dependencias

---
+++ progweb-maven-projeto/src/main/java/cditest/Main.java
@@ -0,0 +1,12 @@
+package cditest;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Pedidos pedidos = new Pedidos();
+        
+        RelatorioService relatorioService = new RelatorioService();
+        //relatorioService.setPedidos(pedidos);        
+    }
+
+}
---
+++ progweb-maven-projeto/src/main/java/cditest/Pedidos.java
@@ -0,0 +1,11 @@
+package cditest;
+
+import java.math.BigDecimal;
+
+public class Pedidos {
+        
+    public BigDecimal totalPedidosMesAtual() {
+        return new BigDecimal("100");
+    }
+
+}
